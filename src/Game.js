53f2e34db974aba4676052f35faa22cce415bb60
mikejeter <mjeter45@yahoo.com>
2020-07-25 11:50:56 -0400

    Added the onClick event handler to place and remove a cell

--- src/Game.js
+++ src/Game.js
@@ -6,7 +6,20 @@ const CELL_SIZE = 20;
 const WIDTH = 800;
 const HEIGHT = 600;
 
+class Cell extends React.Component {
 
+    render() {
+        const { x, y } = this.props;
+        return (
+            <div className="Cell" style={{
+                left: `${CELL_SIZE * x + 1}px`,
+                top: `${CELL_SIZE * y + 1}px`,
+                width: `${CELL_SIZE - 1}px`,
+                height: `${CELL_SIZE - 1}px`,
+            }} />
+        );
+    }
+}
 
 class Game extends React.Component {
   
@@ -35,12 +48,24 @@ class Game extends React.Component {
 
       return board;
     }
+
+    getElementOffset() {
+      const rect = this.boardRef.getBoundingClientRect();
+      const doc = document.documentElement;
+
+      return {
+        x: (rect.left + window.pageXOffset) - doc.clientLeft,
+        y: (rect.top + window.pageYOffset) - doc.clientTop,
+      }
+    }
+
+
     // Create cells from this.board
     makeCells() {
       let cells = [];
       for (let y = 0; y < this.rows;y++) {
         for (let x = 0; x < this.cols;x++) {
-          if (board[y][x]) {
+          if (this.board[y][x]) {
             cells.push({ x, y});
           }
         }
@@ -49,17 +74,49 @@ class Game extends React.Component {
       return cells;
     }
 
+    handleClick = (event) => {
 
+        const elemOffset = this.getElementOffset();
+        const offsetX = event.clientX - elemOffset.x;
+        const offsetY = event.clientY - elemOffset.y;
+        
+        const x = Math.floor(offsetX / CELL_SIZE);
+        const y = Math.floor(offsetY / CELL_SIZE);
 
-
-     render() {    
-       return (   
-       <div>
-           <div className="Board"  
-           style={{ width: WIDTH, height: HEIGHT,    backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}></div>      
-            </div>    
-            );  
+        if (x >= 0 && x <= this.cols && y >= 0 && y <= this.rows) {
+            this.board[y][x] = !this.board[y][x];
         }
+
+        this.setState({ cells: this.makeCells() });
+    }
+
+
+     render() {
+        const { cells, interval, isRunning } = this.state;
+        return (
+            <div>
+                <div className="Board"
+                    style={{ width: WIDTH, height: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}
+                    onClick={this.handleClick}
+                    ref={(n) => { this.boardRef = n; }}>
+
+                    {cells.map(cell => (
+                        <Cell x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`}/>
+                    ))}
+                </div>
+
+                <div className="controls">
+                    Update every <input value={this.state.interval} onChange={this.handleIntervalChange} /> msec
+                    {isRunning ?
+                        <button className="button" onClick={this.stopGame}>Stop</button> :
+                        <button className="button" onClick={this.runGame}>Run</button>
+                    }
+                    <button className="button" onClick={this.handleRandom}>Random</button>
+                    <button className="button" onClick={this.handleClear}>Clear</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Game;
